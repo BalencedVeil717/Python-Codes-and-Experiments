13623f423d8e0d9fe9a7d8263b4128aea65c0d55
BalencedVeil717 <abhinavkumar150807@gmail.com>
2024-02-04 14:59:05 +0530

    Update Dictionary.py

--- Projects/Tools/Dictionary.py
+++ Projects/Tools/Dictionary.py
@@ -35,3 +35,30 @@ dotX3()
 d = dictionary.meaning(s)
 properPrint(d)
 
+# RESULT
+
+# DICTIONARY
+
+
+# Enter Word : wind
+# Finding...
+
+# [ NOUN ]
+# -  Air moving (sometimes with considerable force
+# -  A tendency or force that influences events
+# -  Breath
+# -  Empty rhetoric or insincere or exaggerated talk
+# -  An indication of potential opportunity
+# -  A musical instrument in which the sound is produced by an enclosed column of air that is moved by bellows or the human breath
+# -  A reflex that expels intestinal gas through the anus
+# -  The act of winding or twisting
+
+# [ VERB ]
+# -  To move or cause to move in a sinuous, spiral, or circular course
+# -  Extend in curves and turns
+# -  Arrange or or coil around
+# -  Catch the scent of; get wind of
+# -  Coil the spring of (some mechanical device
+# -  Form into a wreath
+# -  Raise or haul up with or as if with mechanical help
+
