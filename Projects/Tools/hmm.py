eeac41c432a184dd55e57bdc60e003ef393ee7a7
BalencedVeil717 <abhinavkumar150807@gmail.com>
2024-02-04 14:47:45 +0530

    Create hmm.py

---
+++ Projects/Tools/hmm.py
@@ -0,0 +1,21 @@
+d = {
+    "Noun": [
+        "air moving (sometimes with considerable force",
+        "a tendency or force that influences events",
+        "breath",
+        "empty rhetoric or insincere or exaggerated talk",
+        "an indication of potential opportunity",
+        "a musical instrument in which the sound is produced by an enclosed column of air that is moved by bellows or the human breath",
+        "a reflex that expels intestinal gas through the anus",
+        "the act of winding or twisting",
+    ],
+    "Verb": [
+        "to move or cause to move in a sinuous, spiral, or circular course",
+        "extend in curves and turns",
+        "arrange or or coil around",
+        "catch the scent of; get wind of",
+        "coil the spring of (some mechanical device",
+        "form into a wreath",
+        "raise or haul up with or as if with mechanical help",
+    ],
+}
